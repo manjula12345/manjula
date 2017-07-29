c4a1bd7fc905d58ea28f9749361f63236c1b29fb
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-07-29 16:57:04 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if(num>n)
+{
+printf("number is positive");
+}
+else if(num<0)
+{
+printf("number is negative");
+}
+else
+{
+printf("num is zero");
+}
+return0;
+}
